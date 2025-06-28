3b1e0d3de02566f78bcf75127b006a51f2274a9f
simocompsci <hamids9alo7777@gmail.com>
2025-06-28 01:12:18 +0100

    my first day learning java

--- Java/Exercises/HelloWorld.java
+++ Java/Exercises/HelloWorld.java
@@ -1,5 +1,61 @@
+import java.util.Scanner;
+
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World! My name is Mohammed and today i will start learning java");
+        // int age = 20;
+        // double gpa = 3.8;
+        // char grade = 'A';
+        // boolean isStudent = true;
+        // String name = "mohammed";
+
+        // System.out.print("hello world\n");
+        // System.out.print("i like bodybuilding!\n");
+        // System.out.println("my age is " + age + " and i got a " +gpa + " gpa in my cs
+        // degree , \n with the grade :" + grade + " in all of my exams");
+        // System.out.println("my name is " + name);
+
+        // if (isStudent) {
+        // System.out.println("you are a student");
+
+        // }
+        // else{
+        // System.out.println("your are not a student");
+
+        // }
+
+        // Scanner scanner = new Scanner(System.in);
+        // System.out.print("enter your name: ");
+        // String askName = scanner.nextLine();
+        // System.out.print("what s your age: ");
+        // int age = scanner.nextInt();
+        // System.out.println("welcome to our school Mr." + askName);
+        // System.out.println("you are " + age + " years old");
+        // // there is other methods like nextline and nextint . EX: nextdouble , nextBoolean...
+
+        // scanner.close();
+
+        // Scanner scanner = new Scanner(System.in);
+        // System.out.print("kubernets is an: ");
+        // scanner.nextLine();
+        // System.out.print(". that helps us manage our : ");
+        // scanner.nextLine();
+        // System.out.print(". it was developped by : ");
+        // scanner.nextLine();
+        // System.out.print(". its relly used in : ");
+        // scanner.nextLine();
+        // System.out.print(". we can find it in : ");
+        // scanner.nextLine();
+        // System.out.print(" cloud service: ");
+        // scanner.nextLine();
+    
+        // scanner.close();
+
+
+        int x = 10;
+        double y = 2.3;
+
+        double result = x % y;
+        System.out.println(result);
+
     }
 }
