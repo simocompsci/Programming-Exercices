1be949edcac42b11b4b0e0ee98eed6a0f61673b9
simocompsci <hamids9alo7777@gmail.com>
2025-06-29 21:25:23 +0100

    some more revision in java

--- Java/Exercises/HelloWorld.java
+++ Java/Exercises/HelloWorld.java
@@ -1,4 +1,6 @@
+import java.util.Random;
 import java.util.Scanner;
+import java.math.*;
 
 public class HelloWorld {
     public static void main(String[] args) {
@@ -51,11 +53,57 @@ public class HelloWorld {
         // scanner.close();
 
 
-        int x = 10;
-        double y = 2.3;
+        // int x = 10;
+        // double y = 2.3;
 
-        double result = x % y;
-        System.out.println(result);
+        // double result = x % y;
+        // System.out.println(result);
+
+        // Scanner scanner = new Scanner(System.in);
+        // System.out.print("what item do you want");
+        // String item = scanner.nextLine();
+        // System.out.print("how much does it cost");
+        // double price = scanner.nextDouble();
+        // System.out.print("how many " + item + " do you want");
+        // int quantity = scanner.nextInt();
+        // double total = price * quantity;
+
+
+        // System.out.println("You have bought " + quantity + " " + item +"s");
+        // System.out.println("Your total is " + total);
+
+
+        // scanner.close();
+
+        // Random random = new Random();
+        // int number = random.nextInt(3 , 34);  for these methods of next.. they are the same
+        // System.out.print("your random number is: " + number);
+
+
+        // Scanner scanner = new Scanner(System.in);
+        // System.out.print("Whats the value of the first side?");
+        // double side1 = scanner.nextDouble();
+        // System.out.print("Whats the value of the second side?");
+        // double side2 = scanner.nextDouble();
+        // scanner.close();
+        // double Hypothenuos = Math.sqrt((Math.pow(side1, 2))+ (Math.pow(side2, 2)));
+
+        // System.out.printf("the Hypothenuos is :%f cm\n" , Hypothenuos);
+
+
+        // in this chapter we will talk about printf
+        String name = "spongbob";
+        int age = 34;
+        double salary = 2222.506666;
+        char gpa = 'A';
+        boolean isEmployed = true;
+
+        System.out.printf("helllllllllllo %s \n" , name);
+        System.out.printf("your age is :  %d \n" , age);
+        System.out.printf("your salary is %.1f \n" , salary);
+        System.out.printf("your gpa is  %c \n" , gpa);
+        System.out.printf("is spongbob employed , %b \n" , isEmployed);
+
 
     }
 }
--- JavaScript/Exercises/Revision/index.js
+++ JavaScript/Exercises/Revision/index.js
@@ -1,29 +1,29 @@
-var message = ' is one of my favorite things to do';
+// var message = ' is one of my favorite things to do';
 
-const hobbies =[
-    'gaming',
-    'sports', 
-    'traveling',
-    'boxing',
-    'manga',
-];
-const name = 'Mohammed';
-const age = '21';
+// const hobbies =[
+//     'gaming',
+//     'sports', 
+//     'traveling',
+//     'boxing',
+//     'manga',
+// ];
+// const name = 'Mohammed';
+// const age = '21';
 
-hobbies.map((hobbie) =>(
-    console.log(hobbie + message)
-));
-const bigInt = 1234567890123456789012345678901234567890n;
+// hobbies.map((hobbie) =>(
+//     console.log(hobbie + message)
+// ));
+// const bigInt = 1234567890123456789012345678901234567890n;
 
-console.log(bigInt + bigInt);
+// console.log(bigInt + bigInt);
 
-document.write(`hello my name is ${name} and i am ${age} years old`)
+// document.write(`hello my name is ${name} and i am ${age} years old`)
 
 
-const sayMyName =(name)  =>{
-    console.log(`Your ${name}`)
-};
-sayMyName("Heisenberg");
+// const sayMyName =(name)  =>{
+//     console.log(`Your ${name}`)
+// };
+// sayMyName("Heisenberg");
 
 // let weight = prompt("How much do you wheigh :");
 // let gender = prompt("what gender are you");
@@ -40,6 +40,96 @@ sayMyName("Heisenberg");
 
 // ValueWeight(weight , gender)
 
-const DeleteUser =() =>{
-    confirm("do you really want to delete this user");
-}
+// const DeleteUser =() =>{
+//     confirm("do you really want to delete this user");
+// }
+
+
+// let age = prompt("how old are you");
+
+// let message = (age > 0 )? "hello there": (age > 18)?"youre an adult":(age > 80 )?"you are really old": "fuck off" ;
+// alert(message);
+// alert( alert(1) || 2 || alert(3) );
+
+
+const hobbies = ["football" , "swimming" , "dancing"];
+
+// for(let i = 0 ; i <= hobbies.length ; i++){
+//     console.log(hobbies[i] + "is one of the best things i like to do")
+// }
+// let i = 0;
+// while (i <= hobbies.length -1) {
+//         console.log(hobbies[i] + "is one of the best things i like to do");
+//         i++;
+// }
+
+// let i = 0;
+// do {
+//     document.write(`<h1>${hobbies[i]} is one of the best things i love to do</h1>`);
+//     i++
+// } while (i <= hobbies.length -1);
+
+
+/*in this block of code we give our loop a label so that when we want to break from a bunch of nested loops 
+we break from the one that has a label*/
+
+
+// outer: for (let i = 0; i < 3; i++) {
+
+//   for (let j = 0; j < 3; j++) {
+
+//     let input = prompt(`Value at coords (${i},${j})`, '');
+
+//     // if an empty string or canceled, then break out of both loops
+//     if (!input) break outer; // (*)
+
+//     // do something with the value...
+//   }
+// }
+
+// alert('Done!');
+// const day = "Friday";
+
+// switch (day) {
+//     case "Monday":
+//         console.log("i hate my life");
+        
+//         break;
+//     case "Friday":
+//         console.log("i love my life");
+//         break;
+
+
+//     default:
+//         console.log("the cycle keeps repeating");
+//         break;
+// }
+
+// function noTextGiven(){
+//     return "no text given you dumbass"
+// }
+// function showText (name , text = noTextGiven()){
+//     return console.log(`My name is ${name} and the text is ${text}`);
+// }
+
+// showText("hassan" , "heloo my name is soozie sozie baloti hhhhhhh");
+
+// function showPrimes(n) {
+
+//   for (let i = 2; i < n; i++) {
+//     if (!isPrime(i)) continue;
+
+//     alert(i);  // a prime
+//   }
+// }
+
+// function isPrime(n) {
+//   for (let i = 2; i < n; i++) {
+//     if ( n % i == 0) return false;
+//   }
+//   return true;
+// }
+
+// showPrimes(60);
+
+
