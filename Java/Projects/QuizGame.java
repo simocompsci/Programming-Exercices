9ce46297fa11782511beb5e15d03ce59fc72a6da
simocompsci <hamids9alo7777@gmail.com>
2025-07-07 20:27:49 +0100

    made two programms one for quizs and one for rock/paper/sissors

--- Java/Projects/QuizGame.java
+++ Java/Projects/QuizGame.java
@@ -1,3 +1,56 @@
+
+import java.util.Scanner;
+
 public class QuizGame {
-    
+
+    public static void main(String[] args) {
+        // declare our variables 
+        String guess;
+        int result = 0;
+
+        // create an array that conatins custom questions
+        String[] questions = {
+            "What is the capital of France?",
+            "Who wrote 'Romeo and Juliet'?",
+            "What planet is known as the Red Planet?",
+            "Which element has the chemical symbol 'O'?",
+            "Who painted the Mona Lisa?"
+        };
+
+        // create a 2D array that contains responses or options
+        String[][] options = {
+            {"A) Paris", "B) Madrid", "C) Berlin", "D) Rome"},
+            {"A) William Shakespeare", "B) Mark Twain", "C) Charles Dickens", "D) Jane Austen"},
+            {"A) Earth", "B) Venus", "C) Mars", "D) Jupiter"},
+            {"A) Hydrogen", "B) Oxygen", "C) Gold", "D) Nitrogen"},
+            {"A) Vincent van Gogh", "B) Pablo Picasso", "C) Leonardo da Vinci", "D) Michelangelo"}
+        };
+
+        String[] answers = {"A", "A", "C", "B", "C"};
+
+        Scanner scanner = new Scanner(System.in);
+
+        // list each question with a loop questions array
+        for (int i = 0; i <= questions.length - 1; i++) {
+            System.out.println(questions[i]);
+            for (String option : options[i]) {
+
+                System.out.println(option);
+
+            }
+            System.out.print("Please enter a guess: ");
+            guess = scanner.next().toUpperCase();
+            if (answers[i].equals(guess)) {
+                result++;
+            }
+
+        }
+        System.out.printf("Your have answerd %d questions correctly", result);
+        scanner.close();
+    }
+
+    // within this loop we list our option (2D array)
+    // get guess from user and check it
+    // we will store the users answer in a guess variable if the guess is correct we increment a value of result if not we do
+    // nothing
 }
--- Java/Projects/RockPaperSissors.java
+++ Java/Projects/RockPaperSissors.java
@@ -1,3 +1,83 @@
+
+import java.util.Random;
+import java.util.Scanner;
+
 public class RockPaperSissors {
-    
+
+    public static void main(String[] args) {
+        // array of strings (rock , paper , siscors) for computer to chose from
+        String[] handsigns = {"rock", "paper", "scissors"};
+        Random random = new Random();
+        int randomNumber;
+        String cpuHand = null;
+        String userHand = null;
+        String answer;
+        boolean wannaPlay = false;
+
+        // we then create a variable that stores a random number between 1-3 and assign each of these numbers to an element of the 
+        // array
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            while (cpuHand == null && userHand == null) {
+                System.out.println("Please chose an option (rock/paper/scissors): ");
+
+                userHand = scanner.next();
+            }
+
+            randomNumber = randomNumber = random.nextInt(1, 4);
+            switch (randomNumber) {
+                case 1:
+                    cpuHand = handsigns[0];
+                    break;
+                case 2:
+                    cpuHand = handsigns[1];
+                    break;
+                case 3:
+                    cpuHand = handsigns[2];
+                    break;
+                default:
+                    System.out.println("nothing");
+
+            }
+
+            if (cpuHand.equals("scissors") && userHand.equals("paper")) {
+                System.out.println("the cpu chosen" + cpuHand);
+                System.out.println("the cpu won");
+            } else if (cpuHand.equals("scissors") && userHand.equals("rock")) {
+                System.out.println("the cpu chosen" + cpuHand);
+                System.out.println("you have won");
+            } else if (cpuHand.equals("paper") && userHand.equals("scissors")) {
+                System.out.println("the cpu chosen" + cpuHand);
+                System.out.println("you have won");
+            } else if (cpuHand.equals("paper") && userHand.equals("rock")) {
+                System.out.println("the cpu chosen" + cpuHand);
+                System.out.println("the cpu won");
+            } else if (cpuHand.equals("rock") && userHand.equals("paper")) {
+                System.out.println("the cpu chosen" + cpuHand);
+                System.out.println("you have won");
+            } else if (cpuHand.equals("rock") && userHand.equals("scissors")) {
+                System.out.println("the cpu chosen" + cpuHand);
+                System.out.println("the cpu won");
+            } else {
+                System.out.println("the cpu chose the same a s you");
+                System.out.println("equal");
+            }
+
+            System.out.println("Do you want to continue playing (yes/no) ? ");
+            answer = scanner.next();
+            if (answer.equals("yes")) {
+                wannaPlay = true;
+                cpuHand = null;
+                userHand = null;
+            } else {
+                wannaPlay = false;
+                break;
+            }
+
+        }
+        scanner.close();
+
+        // test wich thing the user have chosen and wich one the computer did chose by comparing elements and returning wich one won
+    }
+
 }
