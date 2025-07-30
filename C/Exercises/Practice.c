bb4405cf6ca6165eb5906c3873ae84c6314b26d0
simocompsci <hamids9alo7777@gmail.com>
2025-07-30 15:45:39 +0100

    simple ex

--- C/Exercises/Practice.c
+++ C/Exercises/Practice.c
@@ -1,24 +1,42 @@
 #include <stdio.h>
-
-int main(){
-    int taille;
-    int T[taille];
-    do
-    {
-        printf("Enter la taille du tableau des entier !!! \n");
-        scanf("%d" , &taille);
-        for (int i = 0; i < taille; i++)
-        {
-            printf("Enter element %d du  tableau :" , i+1);
-            scanf("%d" , &T[i]);
-        }
-
-        for (int i = 0; i < taille; i++)
-        {
-            printf("%d " , T[i]);
-        }
-        
-        
-    } while (taille <= 0);
-    
+int a = 2, b = 3; /* Declaration des variables globales */
+int main()
+{
+    int F1(int);
+    a++;                   // Je change la valeur de la variable globale a
+    b--;                   // Je change la valeur de la variable globale b
+    printf("a = %d\n", a); /* Affichage de a */
+    printf("b = %d\n", b); /* Affichage de b */
+    b = F1(a);             // Je change les valeurs de a et b
+    printf("a = %d\n", a); /* Affichage de a */
+    printf("b = %d\n", b); /* Affichage de b */
 }
+int F1(int x)
+{
+    a++;
+    return (3 * a + b - x);
+}
+
+// #include <stdio.h>
+
+// int main(){
+//     // int taille;
+//     // int T[taille];
+//     // do
+//     // {
+//     //     printf("Enter la taille du tableau des entier !!! \n");
+//     //     scanf("%d" , &taille);
+//     //     for (int i = 0; i < taille; i++)
+//     //     {
+//     //         printf("Enter element %d du  tableau :" , i+1);
+//     //         scanf("%d" , &T[i]);
+//     //     }
+
+//     //     for (int i = 0; i < taille; i++)
+//     //     {
+//     //         printf("%d " , T[i]);
+//     //     }
+
+//     // } while (taille <= 0);
+
+// }
