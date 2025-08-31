9c1ecbf5cd064f12a74b1a9693d14f6691620600
simocompsci <hamids9alo7777@gmail.com>
2025-08-31 19:51:52 +0100

    recursion

--- C/Exercises/Recursion.c
+++ C/Exercises/Recursion.c
@@ -25,29 +25,72 @@
 // }
 
 
-int printChar(int count){
-    if (count == 0)
+// int printChar(int count){
+//     if (count == 0)
+//     {
+//         return 1;
+//     }
+//     printf("#");
+//     return printChar(count - 1);
+// }
+
+// int mario(int n){
+//     if (n == 0)
+//     {
+//         return 1;
+//     }
+//     mario(n - 1);
+//     printChar(n);
+//     printf("\n");
+//     return 1;
+// }
+
+// prints all natural numbers from 1 to 50
+// int prntN(int n){
+//     if (n == 50)
+//     {
+//         return 50;
+//     }
+//     printf("%d " , n);
+//     return prntN(n + 1);
+    
+// }
+// int main(){
+//     printf("%d " , prntN(1));
+//     return 0;
+// }
+
+
+// int sumRec(int n){
+//     if (n == 1)
+//     {
+//         return 1;
+//     }
+//     return n + sumRec(n - 1);
+    
+// }
+
+//  int main(){
+//     printf("%d\n" , sumRec(5));
+//     return 0;
+// }
+
+int PrintArr(int* arr , int n , int size){
+    if (n == size)
     {
-        return 1;
+        return n;
     }
-    printf("#");
-    return printChar(count - 1);
+    printf("%d\n" , arr[n]);
+    return PrintArr(arr , n +1 , size);
+    
 }
 
-int mario(int n){
-    if (n == 0)
-    {
-        return 1;
-    }
-    mario(n - 1);
-    printChar(n);
-    printf("\n");
-    return 1;
-}
 
 int main(){
-    int n = 5;  
-    mario(n);
+    int arr[] = {1,2,4,5,7};
+    int size = sizeof(arr)/sizeof(int);
+    int* ptr = arr;
+    int n = 0;
+    PrintArr(ptr , n , size);
     return 0;
 }
-
