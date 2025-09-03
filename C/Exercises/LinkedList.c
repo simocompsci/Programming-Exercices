91790d649605a61b47582b344eef1bad5a69344f
simocompsci <hamids9alo7777@gmail.com>
2025-09-03 22:47:08 +0100

    first linked list emplemented in C

--- C/Exercises/LinkedList.c
+++ C/Exercises/LinkedList.c
@@ -24,6 +24,37 @@ void count_list_nodes(struct node *head){
     
 }
 
+void add_at_end(struct node *head , int data){
+    struct node *ptr , *temp;
+    ptr = head;
+
+    temp = (struct node*)malloc(sizeof(struct node));
+    temp->data = data;
+    temp->link = NULL;
+
+    while (ptr->link != NULL)
+    {
+        ptr = ptr->link;
+    }
+    ptr->link = temp;
+}
+
+void print_list(struct node *head){
+    if (head == NULL)
+    {
+        printf("list is empty!!! \n");
+    }
+    struct node *ptr = NULL;
+    ptr = head;
+    while (ptr != NULL)
+    {
+        printf("%d " , ptr->data);
+        ptr = ptr->link;
+    }
+    
+    
+}
+
 int main(){
     struct node *head = (struct node*)malloc(sizeof(struct node));
     head->data = 99;
@@ -44,7 +75,11 @@ int main(){
     // printf("%d\n" , current->data);
     // printf("%p\n" , current->link);
 
+    add_at_end(head , 48);
+    add_at_end(head , 12);
     count_list_nodes(head);
-    
+    print_list(head);
+    free(head);
+    free(current);
     return 0;
 }
