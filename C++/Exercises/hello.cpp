9483b3f2c8f1083d5bd934fc23b1ed6c2f7b039a
simocompsci <hamids9alo7777@gmail.com>
2025-09-08 18:55:37 +0100

    some practice wit c++

--- C++/Exercises/hello.cpp
+++ C++/Exercises/hello.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+using namespace std;
 
 int main(){
-    std::cout << "Hello World !!!" <<std::endl;
+    cout << "Hello World !!!\n";
+    cout << "My name is mohammed i am 21 years old and i am studying cs as a  degree !!!\n";
+    std::cout << "i want to learn c++ to start making video games , it s a hard thing but i would love to make my own game\n";
+    int count = 0;
+    for (int i = 10; i > count; i--)
+    {
+        cout<< i;
+        cout<< "\n";
+    }
+    cout<< "happy new year\n";
+    int age;
+    cout<< "Enter your age please to confirm :";
+    cin >> age;
+    age >= 18 ? cout<< "You are an adult !!" : cout<< "get the fuck out of here\n";
+
     return 0;
 }
