505fc45cf03b655b7c570358e74068a6806d7226
simocompsci <hamids9alo7777@gmail.com>
2025-09-12 22:35:24 +0100

    twoSum in java

--- Java/Exercises/TwoSum.java
+++ Java/Exercises/TwoSum.java
@@ -1,5 +1,31 @@
+import java.util.Arrays;
+import java.util.HashMap;
+
 public class TwoSum{
     public static void main(String[] args) {
-        
+        int[] arr = {2,7,11,15};
+        int[] result = twoSum(arr , 9);
+        System.out.println("the result is : " + Arrays.toString(result));
+    }
+
+    public static int[] twoSum(int[] nums, int target){
+        int[] result = new int[2];
+        int x , y;
+        HashMap<Integer , Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+        for (Integer key : map.keySet()) {
+            x = key;
+            y = target - x;
+            if(x!= y && map.containsKey(y)){
+                result[0] = map.get(x);
+                result[1] = map.get(y);
+            }
+            else{
+                
+            }
+        }
+        return  result;
     }
 }
