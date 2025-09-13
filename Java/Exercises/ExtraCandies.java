f199eb078516e2b6e72a416ea86ade11f41a3da6
simocompsci <hamids9alo7777@gmail.com>
2025-09-13 22:15:09 +0100

    leet code extraCandies

--- Java/Exercises/ExtraCandies.java
+++ Java/Exercises/ExtraCandies.java
@@ -16,7 +16,6 @@ public class ExtraCandies {
     public static  List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
         int max = Arrays.stream(candies).max().getAsInt();
         ArrayList<Boolean> result = new ArrayList<>();
-        System.err.println(max);
         for (int i = 0; i < candies.length; i++) {
             candies[i] = candies[i] + extraCandies;
         }
