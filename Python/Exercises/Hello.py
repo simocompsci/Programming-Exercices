f8c4869a6cd8825975b01ab9ce3e4b6d4dacf339
simocompsci <hamids9alo7777@gmail.com>
2025-10-28 21:17:25 +0100

    quicksort in python

--- Python/Exercises/Hello.py
+++ Python/Exercises/Hello.py
@@ -1,6 +1,11 @@
-x = int(input("Enter your age :"))
+def QuickSort(arr):
+    if len(arr) < 2:
+        return arr
+    else:
+        pivot = arr[0]
+        less = [i for i in arr[1:] if i <= pivot]
+        greater = [i for i in arr[1:] if i >= pivot]
 
-if x >= 18:
-    print("you are an adult!!")
-else:
-    print("fuck out of here!!")
+        return QuickSort(less) + [pivot] + QuickSort(greater)
+    
+print(QuickSort([10,25,66,3,1,2,44,56,87,96,47,33,14,54]))
