195892adb48ce4ce814ab723d0bf4802213cbbd0
simocompsci <hamids9alo7777@gmail.com>
2025-10-31 19:13:44 +0100

    improved it , beats 99%

--- Python/Exercises/MoveZero.py
+++ Python/Exercises/MoveZero.py
@@ -1,19 +1,28 @@
 def moveZeroes(nums):
-        temp = 0
-        if len(nums) < 2:
-                return nums
-        else:
-                for i in range(len(nums) - 1):
-                        for j in range(len(nums) - 1 - i):
-                            if nums[j] == 0:
-                                    temp = nums[j]
-                                    nums[j] = nums[j+1]
-                                    nums[j+1] = temp
-                            else:
-                                    continue  
-                return nums
+    if len(nums) < 2:
+        return nums
+    else:
+        pivot = 0
+        left = [i for i in nums if i != pivot]
+        right = [i for i in nums if i == pivot]
 
-
-
-numbers = [1,4,0,12,3,0,66,55,88,0,1,0,7,0]
+        nums = left + right
+        return nums
+    
+    
+numbers = [0,1,0,3,12]
 print(moveZeroes(numbers))
+
+ # temp = 0
+        # if len(nums) < 2:
+        #         return nums
+        # else:
+        #         for i in range(len(nums) - 1):
+        #                 for j in range(len(nums) - 1 - i):
+        #                     if nums[j] == 0:
+        #                             temp = nums[j]
+        #                             nums[j] = nums[j+1]
+        #                             nums[j+1] = temp
+        #                     else:
+        #                             continue  
+        #         return nums
