790e2bd66d636ae828f2c5054a2e5f7093149ab5
simocompsci <hamids9alo7777@gmail.com>
2025-10-31 17:08:49 +0100

    found the solution using euclid s algorithm

--- Python/Exercises/GCDStrings.py
+++ Python/Exercises/GCDStrings.py
@@ -1,24 +1,15 @@
 def GCDStrings(str1 , str2):
-    x = ""
-    x += str1[0]
-    for i in range(len(str1) - 1):
-        if str1[0] != str1[i+1]:
-            x += str1[i + 1]
-        else:
-            break
-
-    str1Times = len(str1) // len(x)
-    str2Times = len(str2) // len(x)
-    if str2Times > 1:
-        x *= str2Times
-        return x
-    else:
-        if (x*str1Times) == str1 and (x*str2Times) == str2:
-            return x
-        else:
-            return ""
+    if str1 + str2 != str2 + str1:
+        return ""
+    
+    def gcd(a, b):
+        while b:
+            a, b = b, a % b
+        return a
+    
+    return str1[:gcd(len(str1), len(str2))]
     
 
 
 
-print(GCDStrings("ABABAB" , "ABAB"))
+print(GCDStrings("AAAAAA" , "ABA"))
