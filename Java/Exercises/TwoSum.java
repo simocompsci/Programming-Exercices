965e541c3cde1d5072c8ae1110f2a696ce4649e6
simocompsci <hamids9alo7777@gmail.com>
2025-11-26 22:23:57 +0100

    Two sum in java

--- Java/Exercises/TwoSum.java
+++ Java/Exercises/TwoSum.java
@@ -3,29 +3,24 @@ import java.util.HashMap;
 
 public class TwoSum{
     public static void main(String[] args) {
-        int[] arr = {2,7,11,15};
-        int[] result = twoSum(arr , 9);
-        System.out.println("the result is : " + Arrays.toString(result));
+        int[] arr = {2, 7, 11, 15};
+        int[] result = twoSum(arr, 9);
+        System.out.println("The result is: " + Arrays.toString(result));
     }
 
-    public static int[] twoSum(int[] nums, int target){
-        int[] result = new int[2];
-        int x , y;
-        HashMap<Integer , Integer> map = new HashMap<>();
+    public static int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
         for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+
             map.put(nums[i], i);
         }
-        for (Integer key : map.keySet()) {
-            x = key;
-            y = target - x;
-            if(x!= y && map.containsKey(y)){
-                result[0] = map.get(x);
-                result[1] = map.get(y);
-            }
-            else{
-                
-            }
-        }
-        return  result;
+
+        return new int[] {-1, -1}; 
     }
 }
