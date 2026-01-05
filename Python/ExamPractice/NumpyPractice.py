6f2987e325d7e6d73c71acd3476be89810b71f2a
simocompsci <hamids9alo7777@gmail.com>
2026-01-05 21:32:01 +0100

    testing some functions of numpy

---
+++ Python/ExamPractice/NumpyPractice.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+# matrice_1 = np.array([[1,0,0], [0,1,0], [0,0,1]])
+# matrice_2 = np.array([[1,0,0], [0,1,0], [0,0,1]])
+# somme_matrice = matrice_1 + matrice_2
+# print(matrice_1)
+# print("    +")
+# print(matrice_2)
+# print("    =")
+# print(somme_matrice)
+
+
+
+# Création d'une séquence d'entiers
+arange_array = np.arange(0, 30,7)
+print("Séquence d'entiers:", arange_array)
