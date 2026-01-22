b959cb653a5adfc39648cf8b991f98e1b155b5b6
simocompsci <hamids9alo7777@gmail.com>
2026-01-22 19:38:19 +0100

    encode decode start

---
+++ Python/Exercises/EncodeDecode.py
@@ -0,0 +1,8 @@
+def encode(self, strs: List[str]) -> str:
+    return "hello"
+
+def decode(self, s: str) -> List[str]:
+    return "hi"
+
+
+# this is the problem set of encode decode strings of the neetcode 150
