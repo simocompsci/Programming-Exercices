81f730a038cc15572107697d19b7d913a6580b2a
simocompsci <hamids9alo7777@gmail.com>
2026-01-24 15:03:09 +0100

    understanding the problem

--- Python/Exercises/EncodeDecode.py
+++ Python/Exercises/EncodeDecode.py
@@ -6,3 +6,12 @@ def decode(self, s: str) -> List[str]:
 
 
 # this is the problem set of encode decode strings of the neetcode 150
+
+
+# The algorithm we are trying to write is not an algorithm that takes one string and ecodes and decodes it.
+# what we are trying to achieve is an algorithm that takes a LIST of strings and encodes it to ONE string.
+
+
+# first Approach:
+
+
