585d8fe7da805425b74cfdb9ae6972415b04d846
simocompsci <hamids9alo7777@gmail.com>
2026-02-28 20:30:59 +0000

    working with my own algo on this problem

--- Python/Exercises/ProductExceptSelf.py
+++ Python/Exercises/ProductExceptSelf.py
@@ -1,5 +1,13 @@
-def ProductExceptSelf():
-    return True
+def ProductExceptSelf(nums):
+    product = 1
+    for i in range(len(nums)):
+        if nums[i] == 0:
+            continue
+        product *= nums[i]
+    return product
 
 
-ProductExceptSelf()
+nums = [1,2,3,8]
+nums1 = [1,2,0,-1,3]
+
+print(ProductExceptSelf(nums))
