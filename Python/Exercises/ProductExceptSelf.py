54a49dfb86625d7891b5e16e763fb7df1d4523ba
simocompsci <hamids9alo7777@gmail.com>
2026-02-28 20:45:28 +0000

    product of array except self

--- Python/Exercises/ProductExceptSelf.py
+++ Python/Exercises/ProductExceptSelf.py
@@ -1,13 +1,23 @@
 def ProductExceptSelf(nums):
-    product = 1
-    for i in range(len(nums)):
-        if nums[i] == 0:
-            continue
-        product *= nums[i]
-    return product
+    prod, zero_cnt = 1, 0
+    for num in nums:
+            if num:
+                prod *= num
+            else:
+                zero_cnt +=  1
+    if zero_cnt > 1: return [0] * len(nums)
+
+    res = [0] * len(nums)
+    for i, c in enumerate(nums):
+            if zero_cnt: res[i] = 0 if c else prod
+            else: res[i] = prod // c
+    return res
+
+    
+    
 
 
-nums = [1,2,3,8]
-nums1 = [1,2,0,-1,3]
+# nums = [1,2,3,8]
+# nums1 = [1,2,0,-1,3]
 
-print(ProductExceptSelf(nums))
+# print(ProductExceptSelf(nums))
